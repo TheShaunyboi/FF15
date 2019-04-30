26c22bc54733df8aee58caded5828ff3839c5d53
riteofthearcane <>
2019-04-29 17:41:37 -0700

    add release versions (temp)

---
+++ Release/Ezreal.lua
@@ -0,0 +1,255 @@
+local Ezreal = {}
+local version = 1
+if tonumber(GetInternalWebResult("Ezreal.version")) > version then
+    DownloadInternalFile("Ezreal.lua", SCRIPT_PATH .. "Ezreal.lua")
+    PrintChat("New version:" .. tonumber(GetInternalWebResult("Ezreal.version")) .. " Press F5")
+end
+require "FF15Menu"
+require "utils"
+local DreamTS = require("DreamTS")
+local dmgLib = require("FF15DamageLib")
+
+function OnLoad()
+    if not _G.Prediction then
+        LoadPaidScript(PaidScript.DREAM_PRED)
+    end
+end
+
+function Ezreal:__init()
+    self.q = {
+        speed = 2000,
+        range = 1150,
+        delay = 0.25,
+        width = 125
+    }
+    self.w = {
+        speed = 1700,
+        range = 1150,
+        delay = 0.25,
+        width = 165
+    }
+    self.r = {
+        speed = 2000,
+        range = 2500,
+        delay = 1,
+        width = 325
+    }
+    self.wBuffTarget = nil
+    self:Menu()
+    self.TS =
+        DreamTS(
+        self.menu,
+        {
+            ValidTarget = function(unit)
+                return IsValidTarget(unit)
+            end,
+            Damage = function(unit)
+                return dmgLib:CalculateMagicDamage(myHero, unit, 100)
+            end
+        }
+    )
+    AddEvent(
+        Events.OnTick,
+        function()
+            self:OnTick()
+        end
+    )
+    AddEvent(
+        Events.OnDraw,
+        function()
+            self:OnDraw()
+        end
+    )
+    AddEvent(
+        Events.OnBuffGain,
+        function(obj, buff)
+            self:OnBuffGain(obj, buff)
+        end
+    )
+    AddEvent(
+        Events.OnBuffLost,
+        function(obj, buff)
+            self:OnBuffLost(obj, buff)
+        end
+    )
+    PrintChat("Ezreal loaded")
+    self.font = DrawHandler:CreateFont("Calibri", 10)
+end
+
+function Ezreal:Menu()
+    self.menu = Menu("asdfezreal", "Ezreal")
+    self.menu:checkbox("q", "AutoQ", true, string.byte("T"))
+    self.menu:key("r", "Manual R Key", 0x5A)
+    self.menu:sub("ezrealDraw", "Draw")
+    self.menu.ezrealDraw:checkbox("q", "Q", true)
+    self.menu.ezrealDraw:slider("qa", "Alpha", 1, 255, 150)
+    self.menu.ezrealDraw:slider("q0r", "AutoQ off: Red", 1, 255, 150)
+    self.menu.ezrealDraw:slider("q0g", "AutoQ off: Green", 1, 255, 150)
+    self.menu.ezrealDraw:slider("q0b", "AutoQ off: Blue", 1, 255, 150)
+    self.menu.ezrealDraw:slider("q1r", "AutoQ on: Red", 1, 255, 255)
+    self.menu.ezrealDraw:slider("q1g", "AutoQ on: Green", 1, 255, 150)
+    self.menu.ezrealDraw:slider("q1b", "AutoQ on: Blue", 1, 255, 150)
+end
+
+function Ezreal:OnDraw()
+    if self.menu.ezrealDraw.q:get() then
+        if self.menu.q:get() then
+            DrawHandler:Circle3D(
+                myHero.position,
+                self.q.range,
+                self:Hex(
+                    self.menu.ezrealDraw.qa:get(),
+                    self.menu.ezrealDraw.q1r:get(),
+                    self.menu.ezrealDraw.q1g:get(),
+                    self.menu.ezrealDraw.q1b:get()
+                )
+            )
+        else
+            DrawHandler:Circle3D(
+                myHero.position,
+                self.q.range,
+                self:Hex(
+                    self.menu.ezrealDraw.qa:get(),
+                    self.menu.ezrealDraw.q0r:get(),
+                    self.menu.ezrealDraw.q0g:get(),
+                    self.menu.ezrealDraw.q0b:get()
+                )
+            )
+        end
+    end
+    if self.wBuffTarget then
+        DrawHandler:Circle3D(
+            self.wBuffTarget.position,
+            20,
+            self:Hex(
+                self.menu.ezrealDraw.qa:get(),
+                self.menu.ezrealDraw.q0r:get(),
+                self.menu.ezrealDraw.q0g:get(),
+                self.menu.ezrealDraw.q0b:get()
+            )
+        )
+    end
+end
+
+function Ezreal:CastQ(target)
+    if myHero.spellbook:CanUseSpell(0) == 0 then
+        local pred = _G.Prediction.GetPrediction(target, self.q, myHero)
+        if
+            pred and pred.castPosition and GetDistanceSqr(pred.castPosition) <= self.w.range * self.w.range and
+                (pred.realHitChance == 1 or _G.Prediction.WaypointManager.ShouldCast(target)) and
+                not pred:windWallCollision() and
+                not pred:minionCollision()
+         then
+            myHero.spellbook:CastSpell(0, pred.castPosition)
+            return true
+        end
+    end
+end
+function Ezreal:CastW(target)
+    if myHero.spellbook:CanUseSpell(1) == 0 then
+        local pred = _G.Prediction.GetPrediction(target, self.w, myHero)
+        if
+            pred and pred.castPosition and GetDistanceSqr(pred.castPosition) <= self.w.range * self.w.range and
+                (pred.realHitChance == 1 or _G.Prediction.WaypointManager.ShouldCast(target)) and
+                not pred:windWallCollision()
+         then
+            myHero.spellbook:CastSpell(1, pred.castPosition)
+            return true
+        end
+    end
+end
+
+function Ezreal:CastR(target)
+    if myHero.spellbook:CanUseSpell(3) == 0 then
+        local pred = _G.Prediction.GetPrediction(target, self.r, myHero)
+        if
+            pred and pred.castPosition and GetDistanceSqr(pred.castPosition) <= self.r.range * self.r.range and
+                (pred.realHitChance == 1 or _G.Prediction.WaypointManager.ShouldCast(target)) and
+                not pred:windWallCollision()
+         then
+            myHero.spellbook:CastSpell(3, pred.castPosition)
+            return true
+        end
+    end
+end
+
+function Ezreal:R(target)
+    if
+        target.buffManager:HasBuffOfType(5) or target.buffManager:HasBuffOfType(8) or
+            target.buffManager:HasBuffOfType(24) or
+            target.buffManager:HasBuffOfType(11) or
+            target.buffManager:HasBuffOfType(22) or
+            target.buffManager:HasBuffOfType(21)
+     then
+        self:CastR(target)
+        return true
+    end
+    local pred = _G.Prediction.GetPrediction(target, self.r, myHero)
+    if
+        pred and pred.realHitChance > 0 and pred.castPosition and
+            (pred.realHitChance == 1 or _G.Prediction.WaypointManager.ShouldCast(target)) and
+            pred:heroCollision(2) and
+            not pred:windWallCollision()
+     then
+        myHero.spellbook:CastSpell(3, pred.castPosition)
+        return true
+    end
+end
+
+function Ezreal:OnTick()
+    if not LegitOrbwalker:IsAttacking() then
+        local targets = self.TS:getAll(myHero, self.r.range)
+        for i = 1, #targets do
+            local target = targets[i]
+            if self.menu.r:get() then
+                if self:CastR(target) then
+                    return
+                end
+            end
+            if LegitOrbwalker:GetMode() == "Combo" then
+                if self:R(target) then
+                    return
+                end
+                local wTarget = self.TS:get(myHero, self.w.range)
+                if i == 1 and wTarget and self:CastW(wTarget) then
+                    return
+                end
+                if self.wBuffTarget and IsValidTarget(self.wBuffTarget, self.q.range) and self:CastQ(self.wBuffTarget) then
+                    return
+                end
+                if self:CastQ(target) then
+                    return
+                end
+            elseif self.menu.q:get() and not _G.Prediction.IsRecalling(myHero) then
+                if self.wBuffTarget and IsValidTarget(self.wBuffTarget, self.q.range) then
+                    self:CastQ(self.wBuffTarget)
+                end
+                if self:CastQ(target) then
+                    return
+                end
+            end
+        end
+    end
+end
+
+function Ezreal:OnBuffGain(obj, buff)
+    if obj and obj.team ~= myHero.team and obj.type == myHero.type and buff.name == "ezrealwattach" then
+        self.wBuffTarget = obj
+        LegitOrbwalker:SetForcedTarget(obj)
+    end
+end
+
+function Ezreal:OnBuffLost(obj, buff)
+    if obj and obj.team ~= myHero.team and obj.type == myHero.type and buff.name == "ezrealwattach" then
+        self.wBuffTarget = nil
+        LegitOrbwalker:UnsetForcedTarget()
+    end
+end
+
+function Ezreal:Hex(a, r, g, b)
+    return string.format("0x%.2X%.2X%.2X%.2X", a, r, g, b)
+end
+
+if myHero.charName == "Ezreal" then
+    Ezreal:__init()
+end
---
+++ Release/Xerath.lua
@@ -0,0 +1,360 @@
+local Xerath = {}
+local version = 1.1
+if tonumber(GetInternalWebResult("Xerath.version")) > version then
+    DownloadInternalFile("Xerath.lua", SCRIPT_PATH .. "Xerath.lua")
+    PrintChat("New version:" .. tonumber(GetInternalWebResult("Xerath.version")) .. " Press F5")
+end
+require "FF15Menu"
+require "utils"
+local DreamTS = require("DreamTS")
+local dmgLib = require("FF15DamageLib")
+
+function OnLoad()
+        if not _G.Prediction then
+        LoadPaidScript(PaidScript.DREAM_PRED)
+    end
+end
+
+function Xerath:__init()
+    self.q = {
+        type = "linear",
+        last = nil,
+        min = 750,
+        max = 1450,
+        charge = 1.5,
+        range = 1450,
+        delay = 0.6,
+        width = 145,
+        speed = math.huge
+    }
+    self.w1 = {
+        type = "circular",
+        range = 1000,
+        delay = 0.83,
+        radius = 250,
+        speed = math.huge
+    }
+    self.w2 = {
+        type = "circular",
+        range = 1000,
+        delay = 0.83,
+        radius = 123,
+        speed = math.huge
+    }
+    self.e = {
+        type = "linear",
+        range = 1000,
+        delay = 0.25,
+        width = 125,
+        speed = 1400
+    }
+    self.r = {
+        type = "circular",
+        active = false,
+        range = 80000,
+        delay = 0.66,
+        radius = 200,
+        speed = math.huge
+    }
+
+    self:Menu()
+    self.TS =
+        DreamTS(
+        self.menu,
+        {
+            ValidTarget = function(unit)
+                return IsValidTarget(unit)
+            end,
+            Damage = function(unit)
+                return dmgLib:CalculateMagicDamage(myHero, unit, 100)
+            end
+        }
+    )
+    AddEvent(
+        Events.OnTick,
+        function()
+            self:OnTick()
+        end
+    )
+    --[[
+    AddEvent(
+        Events.OnBuffGain,
+        function(obj, buff)
+            self:OnBuffGain(obj, buff)
+        end
+    )
+    AddEvent(
+        Events.OnBuffLost,
+        function(obj, buff)
+            self:OnBuffLost(obj, buff)
+        end
+    )
+    ]]
+    AddEvent(
+        Events.OnNewPath,
+        function(obj, paths, isWalk, dashspeed)
+            self:OnNewPath(obj, paths, isWalk, dashspeed)
+        end
+    )
+    AddEvent(
+        Events.OnDraw,
+        function()
+            self:OnDraw()
+        end
+    )
+    PrintChat("Xerath loaded")
+    self.font = DrawHandler:CreateFont("Calibri", 10)
+end
+
+function Xerath:Menu()
+    self.menu = Menu("asdfxerath", "Xerath")
+    self.menu:slider("wc", "W Center Hitchance", 1, 100, 50)
+    self.menu:slider("e", "E Hitchance", 1, 100, 50)
+
+    self.menu:slider("rr", "R Near Mouse Radius", 0, 3000, 1500)
+    self.menu:sub("antigap", "Anti Gapclose")
+    for _, enemy in ipairs(ObjectManager:GetEnemyHeroes()) do
+        self.menu.antigap:checkbox(enemy.charName, enemy.charName, true)
+    end
+    self.menu:key("tap", "Tap Key", string.byte("T"))
+    self.menu:sub("xerathDraw", "Draw")
+    self.menu.xerathDraw:sub("q", "Q")
+    self.menu.xerathDraw.q:checkbox("q", "Q", true)
+    self.menu.xerathDraw.q:slider("qa", "Alpha", 1, 255, 150)
+    self.menu.xerathDraw.q:slider("qr", "Red", 1, 255, 150)
+    self.menu.xerathDraw.q:slider("qg", "Green", 1, 255, 150)
+    self.menu.xerathDraw.q:slider("qb", "Blue", 1, 255, 150)
+    self.menu.xerathDraw:sub("r", "R")
+    self.menu.xerathDraw.r:checkbox("r", "R", true)
+    self.menu.xerathDraw.r:checkbox("rmini", "R Minimap", true)
+    self.menu.xerathDraw.r:slider("ra", "Alpha", 1, 255, 150)
+    self.menu.xerathDraw.r:slider("rr", "Red", 1, 255, 150)
+    self.menu.xerathDraw.r:slider("rg", "Green", 1, 255, 150)
+    self.menu.xerathDraw.r:slider("rb", "Blue", 1, 255, 150)
+end
+
+function Xerath:OnDraw()
+    if self.menu.xerathDraw.q.q:get() then
+        local isQActive, remainingTime = self:IsQActive()
+        local range = self.q.max
+
+        if isQActive then
+            range = self:GetQRange(remainingTime)
+        end
+
+        DrawHandler:Circle3D(
+            myHero.position,
+            range,
+            self:Hex(
+                self.menu.xerathDraw.q.qa:get(),
+                self.menu.xerathDraw.q.qr:get(),
+                self.menu.xerathDraw.q.qg:get(),
+                self.menu.xerathDraw.q.qb:get()
+            )
+        )
+    end
+    color =
+        self:Hex(
+        self.menu.xerathDraw.r.ra:get(),
+        self.menu.xerathDraw.r.rr:get(),
+        self.menu.xerathDraw.r.rg:get(),
+        self.menu.xerathDraw.r.rb:get()
+    )
+    if myHero.spellbook:Spell(3).level > 0 then
+        self.r.range = 2000 + 1200 * myHero.spellbook:Spell(3).level
+        if self.menu.xerathDraw.r.r:get() then
+            DrawHandler:Circle3D(myHero.position, self.r.range, color)
+        end
+        if self.menu.xerathDraw.r.rmini:get() then
+            local radius = TacticalMap.width * self.r.range / 14692
+            DrawHandler:Circle(TacticalMap:WorldToMinimap(myHero.position), self.r.range, color)
+        end
+    end
+    if self:IsRActive() then
+        DrawHandler:Circle3D(pwHud.hudManager.virtualCursorPos, self.menu.rr:get(), color)
+    end
+end
+
+function Xerath:CastQ(target)
+    local isQActive, remainingTime = self:IsQActive()
+
+    if myHero.spellbook:CanUseSpell(0) == 0 then
+        self.q.range = isQActive and self:GetQRange(remainingTime) or self.q.max
+
+        local pred = _G.Prediction.GetPrediction(target, self.q, myHero)
+        if
+            pred.castPosition and (pred.realHitChance == 1 or _G.Prediction.WaypointManager.ShouldCast(target)) and
+                GetDistanceSqr(pred.castPosition) <= self.q.range * self.q.range
+         then
+            if isQActive then
+                myHero.spellbook:UpdateChargeableSpell(0, pred.castPosition, true)
+                return true
+            else
+                myHero.spellbook:CastSpell(0, pred.castPosition)
+            end
+        end
+    end
+end
+
+function Xerath:CastW1(target)
+    if myHero.spellbook:CanUseSpell(1) == 0 then
+        local pred = _G.Prediction.GetPrediction(target, self.w1, myHero)
+        if
+            pred and pred.castPosition and (pred.realHitChance == 1 or _G.Prediction.WaypointManager.ShouldCast(target)) and
+                GetDistanceSqr(pred.castPosition) <= self.w1.range * self.w1.range
+         then
+            myHero.spellbook:CastSpell(1, pred.castPosition)
+            return true
+        end
+    end
+end
+
+function Xerath:CastW2(target)
+    if myHero.spellbook:CanUseSpell(1) == 0 then
+        local pred = _G.Prediction.GetPrediction(target, self.w2, myHero)
+        if
+            pred and pred.castPosition and pred.hitChance >= self.menu.wc:get() / 100 and
+                (pred.realHitChance == 1 or _G.Prediction.WaypointManager.ShouldCast(target)) and
+                GetDistanceSqr(pred.castPosition) <= self.w2.range * self.w2.range
+         then
+            myHero.spellbook:CastSpell(1, pred.castPosition)
+            return true
+        end
+    end
+end
+
+function Xerath:CastE(target)
+    if myHero.spellbook:CanUseSpell(2) == 0 then
+        local pred = _G.Prediction.GetPrediction(target, self.e, myHero)
+        if
+            pred and pred.castPosition and (pred.realHitChance == 1 or _G.Prediction.WaypointManager.ShouldCast(target)) and
+                not pred:windWallCollision() and
+                not pred:minionCollision() and
+                pred.hitChance >= self.menu.e:get() / 100 and
+                GetDistanceSqr(pred.castPosition) <= self.e.range * self.e.range
+         then
+            myHero.spellbook:CastSpell(2, pred.castPosition)
+            return true
+        end
+    end
+end
+
+function Xerath:CastR()
+    self.r.range = 2000 + 1200 * myHero.spellbook:Spell(3).level
+    local target =
+        LegitOrbwalker:GetTarget(self.menu.rr:get(), "AP", pwHud.hudManager.virtualCursorPos) or
+        LegitOrbwalker:GetTarget(range, "AP", myHero)
+    if myHero.spellbook:CanUseSpell(3) == 0 and target and self.menu.tap:get() then
+        local pred = _G.Prediction.GetPrediction(target, self.r, myHero)
+        if
+            pred and pred.castPosition and (pred.realHitChance == 1 or _G.Prediction.WaypointManager.ShouldCast(target)) and
+                GetDistanceSqr(pred.castPosition) <= self.r.range * self.r.range
+         then
+            myHero.spellbook:CastSpell(3, pred.castPosition)
+            return true
+        end
+    end
+end
+
+function Xerath:OnTick()
+    local qActive = self:IsQActive()
+    local rActive = self:IsRActive()
+
+    if qActive or rActive then
+        LegitOrbwalker:BlockAttack(true)
+    else
+        LegitOrbwalker:BlockAttack(false)
+    end
+
+    if rActive then
+        if self:CastR() then
+            return
+        end
+    end
+
+    local targets = self.TS:update()
+    for i = 1, #targets do
+        local target = targets[i]
+        if self.menu.antigap[target.charName] and self.menu.antigap[target.charName]:get() then
+            _, canHit = _G.Prediction.IsDashing(target, self.e, myHero)
+            if canHit then
+                self:CastE(target)
+            end
+        end
+        if LegitOrbwalker:GetMode() == "Combo" then
+            if self:CastE(target) then
+                return
+            end
+
+            if self:CastW2(target) then
+                return
+            end
+
+            if self:CastW1(target) then
+                return
+            end
+
+            if self:CastQ(target) then
+                return
+            end
+        elseif LegitOrbwalker:GetMode() == "Harass" then
+            self:CastQ(target)
+        end
+    end
+end
+
+function Xerath:IsQActive()
+    local buffs = myHero.buffManager.buffs
+
+    for i = 1, #buffs do
+        local buff = buffs[i]
+
+        if buff.name == "XerathArcanopulseChargeUp" then
+            return true, buff.remainingTime
+        end
+    end
+
+    return false, 0
+end
+
+function Xerath:IsRActive()
+    local buffs = myHero.buffManager.buffs
+
+    for i = 1, #buffs do
+        local buff = buffs[i]
+
+        if buff.name == "XerathLocusOfPower2" then
+            return true, buff.remainingTime
+        end
+    end
+
+    return false, 0
+end
+
+function Xerath:GetQRange(remainingTime)
+    local chargeStart = RiotClock.time + remainingTime - 4
+    return math.min(
+        self.q.min + (self.q.max - self.q.min) * (RiotClock.time - chargeStart - 0.2) / self.q.charge,
+        self.q.max
+    )
+end
+
+function Xerath:OnNewPath(obj, paths, isWalk, dashspeed)
+    --[[  if
+        myHero.spellbook:CanUseSpell(2) == 0 and obj and not isWalk and IsValidTarget(obj) and
+            self.menu.antigap[obj.charName] and
+            self.menu.antigap[obj.charName]:get() and
+            GetDistance(obj.position) <= self.e.range and
+            dashspeed ~= 0
+     then
+        self:CastE(obj)
+    end ]]
+end
+
+function Xerath:Hex(a, r, g, b)
+    return string.format("0x%.2X%.2X%.2X%.2X", a, r, g, b)
+end
+
+if myHero.charName == "Xerath" then
+    Xerath:__init()
+end
