60232224d8bc71b4c11e3e528047f3e024120480
riteofthearcane <>
2019-07-04 18:54:36 +0900

    spellblocker

---
+++ spellblocker.lua
@@ -0,0 +1,23 @@
+require "FF15Menu"
+require "utils"
+
+local champions = {
+    ["Sivir"] = {slot = 2},
+    ["Fiora"] = {slot = 1},
+    ["Irelia"] = {slot = 1},
+    ["Nocturne"] = {slot = 1},
+    ["Morgana"] = {slot = 2}
+}
+
+
+--[[
+    Spell Types
+        Evade: the spell is a skillshot that can be dodged by evade
+        Dash: the spell is a damaging spell that involves the enemy dashing - Alistar W
+        
+
+]]
+
+local spells = {
+    --aatrox 
+}
